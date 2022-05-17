fe7cb56fdd2431892136b3bd9af6f5fe110d6d7b
Ali <Alihosseinzadeh1379ahz@gmail.com>
2022-05-17 17:20:11 +0430

    validators added in the serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from django.core.validators import RegexValidator
 
+
+phone_number_regex = RegexValidator(  
+                                    regex=r'^09(1[0-9]|3[0-9])[0-9]{7}$', 
+                                    message="mobile phone number must be in this format '09123456789' and exactly 11 digits."
+                                )
 
 class SendTextSerializer(serializers.Serializer):
-    phone_number = serializers.CharField()
-    email = serializers.CharField()
+    phone_number = serializers.CharField(validators=[phone_number_regex])
+    email = serializers.EmailField()
     text = serializers.CharField()
